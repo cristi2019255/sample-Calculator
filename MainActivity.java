b27bf29ca0495b44b09896411ff1c27a82b33639
Grosu Cristian <50914302+cristi2019255@users.noreply.github.com>
2020-02-05 19:34:11 +0200

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,11 @@
-package com.example.myapplication;
+package com.example.firstandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onButtonClickedAdd(View v ){
+        EditText el1=(EditText)findViewById(R.id.num1);
+        EditText el2=(EditText)findViewById(R.id.num2);
+        TextView rezText=(TextView)findViewById(R.id.result);
+
+        int numb1=Integer.parseInt(el1.getText().toString());
+        int numb2=Integer.parseInt(el2.getText().toString());
+        int rez=numb1+numb2;
+        //int rez=numb1-numb2;
+        rezText.setText(Integer.toString(rez));
+
+    }
+    public void OnButtonClickedSub(View v){
+        EditText el1=(EditText)findViewById(R.id.num1);
+        EditText el2=(EditText)findViewById(R.id.num2);
+        TextView rezText=(TextView)findViewById(R.id.result);
+        int numb1= Integer.parseInt(el1.getText().toString());
+        int numb2=Integer.parseInt(el2.getText().toString());
+        int rez=numb1-numb2;
+        rezText.setText(Integer.toString(rez));
+    }
+
+    public void OnButtonClickedMul(View v){
+        EditText el1=(EditText)findViewById(R.id.num1);
+        EditText el2=(EditText)findViewById(R.id.num2);
+        TextView rezText=(TextView)findViewById(R.id.result);
+        int numb1= Integer.parseInt(el1.getText().toString());
+        int numb2=Integer.parseInt(el2.getText().toString());
+        int rez=numb1*numb2;
+        rezText.setText(Integer.toString(rez));
+    }
+
+    public void OnButtonClickedDiv(View v){
+        EditText el1=(EditText)findViewById(R.id.num1);
+        EditText el2=(EditText)findViewById(R.id.num2);
+        TextView rezText=(TextView)findViewById(R.id.result);
+        int numb1= Integer.parseInt(el1.getText().toString());
+        int numb2=Integer.parseInt(el2.getText().toString());
+        int rez=numb1/numb2;
+        rezText.setText(Integer.toString(rez));
+    }
+    public void OnButtonClickedPower(View v){
+        EditText el1=(EditText)findViewById(R.id.num1);
+        EditText el2=(EditText)findViewById(R.id.num2);
+        TextView rezText=(TextView)findViewById(R.id.result);
+        int numb1= Integer.parseInt(el1.getText().toString());
+        int numb2=Integer.parseInt(el2.getText().toString());
+        int rez=1;
+        for (int i=0;i<numb2;i++){
+            rez*=numb1;
+        }
+
+        rezText.setText(Integer.toString(rez));
+    }
+
+
 }
